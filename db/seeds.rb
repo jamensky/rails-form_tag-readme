19b19deb71643f39a481e2497594ffdd50de5ec5
Jamal <jamensky1991@gmail.com>
2021-04-14 02:21:11 +0100

    commit tree 3c8e487c327807f17104d446ee70076d19f72fe7

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+posts = Post.create([{title:'Van Damme', description: 'biography'}])
